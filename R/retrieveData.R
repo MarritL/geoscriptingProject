ceeba67e4aeca26bf5b86d1733c542fcd0ee29f1
Marrit <marrit.leenstra@wur.nl>
2019-01-24 12:27:35 +0100

    project initiation and retrieving data

---
+++ R/retrieveData.R
@@ -0,0 +1,29 @@
+# Authors: Anne-Juul Welsink and Marrit Leenstra
+# 24th January 2019
+# Exercise 9, Geoscripting, Wageningen University 
+
+# Function to download and unpack data from url to a user-specified data directory.
+# Input: 
+#    url: a url or vector of urls
+#    destDirectory: directory where the data is saved and unzipped
+# Output:
+#    files saved in user-specified data directory. 
+
+retrieveData <- function(url, destDirectory){
+  
+  # Create output folder if needed
+  try(dir.create(destDirectory, showWarnings = FALSE))    
+  
+  filename <- "movementData"
+  
+  # Get path of data directory and specify filename
+  dataDirectory <- list.files(pattern = destDirectory, full.names = TRUE)
+    
+  # Download and unzip files
+  download.file(url = url, destfile = paste0(dataDirectory, filename), mode="wb")
+  unzip(paste0(dataDirectory, filename), exdir = destDirectory, overwrite = TRUE)  
+  
+  # Delete zip file
+  file.remove(paste0(dataDirectory, filename))
+  
+}
