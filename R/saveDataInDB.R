ac96d0c62117c1ce9d3f153af2e7f6d1542ad401
Marrit <marrit.leenstra@wur.nl>
2019-01-25 10:21:05 +0100

    change columnnames of tables in DB, saving works now in main

--- R/saveDataInDB.R
+++ R/saveDataInDB.R
@@ -7,17 +7,19 @@
 #    datapath: full path to the data
 #    database: name of databaseconnection to add data to
 #    tableName: name of table in database to add data to
+#    columnNames: 
 # Output:
 #    data saved in user-specified database. 
 
-saveDataInDB <- function(dataPath, database, tableName){
+saveDataInDB <- function(dataPath, database, tableName, columnNames){
   
+  # read data into dataframe(s)
   DFlist <- lapply(dataPath,read.csv2)
 
+  # save every dataframe in the database
   for (i in 1:length(DFlist)){
-    
-    dbWriteTable(database, tableName[i], DFlist[[i]], append=TRUE)
+    dataFrame <- DFlist[[i]]
+    names(dataFrame) <- columnNames
+    dbWriteTable(database, tableName, dataFrame, append=TRUE)
   }
 }
-
-#https://doc-14-8g-docs.googleusercontent.com/docs/securesc/37bqdm91qlf446h600h8i92iq8or1rtu/pa3r1qi5ua99md2i6cabjk26gi0oe20v/1548345600000/04837304595108072515/04837304595108072515/
