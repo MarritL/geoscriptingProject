1732276a62b07ce095280119ede87402751d69a9
Marrit <marrit.leenstra@wur.nl>
2019-01-29 16:14:43 +0100

    extractRasterValues returns list with matrices per animal, plot profiles works for old extract raster, needs update.

--- R/plotProfiles.R
+++ R/plotProfiles.R
@@ -5,27 +5,37 @@
 # Function to plot the altitude of the simulations and the animal
 #
 # Input: 
-#   simulations:
-#    
+#   simulations: dataframe with extracted raster values per point location as output from extractRasterValues
+#   trajectories: dataframe with extracted raster values per point location  
 # Output:
 #    
 
 plotProfiles <- function(simulations, trajectories){
   
   simulations <- simulationElevations
+  
+  
+  # make vector with different animals to loop over
   animalNames <- unique(simulations$animal)
   
-  plot.new()
   for (animalname in animalNames){
-    print(animalname)
-    X11()
+    # filter the simulations and trajetory based on animalname
+    realAnimal <- trajectories %>% filter(animal == animalname)
     simulationsAnimal <- simulations %>% filter(animal == animalname)
-    numberSimulations <- unique(simulationsAnimal$nsim)
+    
+    # plot real trajectory
+    plot(realAnimal[,'rasterVal'], col = 'red', type = 'l')
+    
+    # make vector with number of simulations to loop over
+    numberSimulations <- unique(simulationsAnimal$nSim)
     
     for (simulation in numberSimulations){
-      print(simulation)
-      oneSimulation <- simulationsAnimal %>% filter(nSim == numberSimulations)
-      plot(oneSimulation[,'ID'], oneSimulation[,'DEM_be75'], type = 'l', col = 'grey', add = TRUE)
+      # filter the simulations based on number of the simulation
+      oneSimulation <- simulationsAnimal %>% filter(nSim == simulation)
+      # add to plot
+      lines(oneSimulation[,'rasterVal'], col = 'grey')
     }
+    # add line of real trajectory in red on top
+    lines(realAnimal[,'rasterVal'], col = 'red', type = 'l')
   }
 }
