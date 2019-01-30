c1517cc1d98aa00e4fa36980b64d05d4e3851d5e
Marrit <marrit.leenstra@wur.nl>
2019-01-30 11:17:38 +0100

    added comments and metadata to boxplot function

--- R/boxplotProfiles.R
+++ R/boxplotProfiles.R
@@ -7,15 +7,16 @@
 # Input: 
 #   simulations: list with matrices with extracted raster values per point location as output from combineSimTrack
 #   animalNames: vector with animalNames
+#   xlab: label for x-axis, default is no label
+#   ylab: label for y-axis, default is no label
+#   title: title for plot, default is no title
+#   addName: optional: add name for every animal at and of title name
 # Output:
-#   plot of simulated trajectories in grey and real trajectory in red per animal
+#   list with boxplot of simulated trajectories and recorded trajectory per animal
 
-boxplotProfiles <- function(simulations, animalNames, xlab, ylab, title, addName = FALSE){
+boxplotProfiles <- function(simulations, animalNames, xlab = NULL, ylab = NULL, title = NULL, addName = FALSE){
   
-  #simulations <- simElCombi
-  #xlab <-  "Trajectory"
-  #ylab <- "Elevation (m)"
-  #title <- "Comparison of recorded and simulated GPS trajectories of"
+  # title base to add animal name to if addName = TRUE
   titlebase <- title
   
   # create empty list
@@ -23,17 +24,18 @@ boxplotProfiles <- function(simulations, animalNames, xlab, ylab, title, addName
   
   for (animal in 1:length(animalNames)){
     
+    # add name to title if addName = TRUE
     if (addName){
       title <- paste(titlebase, animalNames[animal])
     }
     
+    # create stacked dataframe
     DFsimulations <- stack(as.data.frame(simulations[[animal]]))
     
-    plotlist <-  list.append(plotlist, assign(paste0("plt", animal), ggplot(DFsimulations) + geom_boxplot(aes(x = ind, y = values)) + xlab(xlab) + ylab(ylab) + ggtitle(title)))
-    
-    #plotlist <- list.append(paste0("plt", animal))
+    # create ggboxplot and add to list
+    plotlist <-  list.append(plotlist, assign(paste0("plt", animal), 
+                      ggplot(DFsimulations) + geom_boxplot(aes(x = ind, y = values)) + 
+                      xlab(xlab) + ylab(ylab) + ggtitle(title)))
   }
-  
   return(plotlist)
-  
 }
