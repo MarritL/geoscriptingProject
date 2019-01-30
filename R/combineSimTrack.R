7584ae12d90545acdf01333f191a72ee9b41706b
Anne-Juul <anne-juul.welsink@wur.nl>
2019-01-30 10:29:57 +0100

    Generalized variable names

--- R/combineSimTrack.R
+++ R/combineSimTrack.R
@@ -7,19 +7,19 @@
 # Input: 
 #   df: a dataframe with raster values per recorded animal location
 #   animalNames: a list of animal names
-#   simEl: matrix with simulated raster values (one simulation per column)  
+#   simVal: matrix with simulated raster values (one simulation per column)  
 # Output: 
 #   matrix with recorded and simulated altitude values (one column per simulation), grouped by animal
-# 
+#    
 
 combineSimTrack <- function(df, animalNames, simEl){
   
   for (animal in 1:length(animalNames)){
     # filter recorded values per animal from dataframe
-    animalEl <- df[df$animal == animalNames[animal],]
+    animalVal <- df[df$animal == animalNames[animal],]
     # add recorded values to simulations matrix
-    simEl[[animal]] <- cbind(animalEl$rasterVal, simEl[[animal]])
+    simVal[[animal]] <- cbind(animalVal$rasterVal, simVal[[animal]])
   }
-  return(simEl)
+  return(simVal)
 }
   
